fa5d3bd7b990913f75153c532a2decd5ea9cdaab
Benjamin <Benjamin@130.254.103.242>
2014-10-24 17:33:53 -0400

    Initial upload

---
+++ SupplyChainCentral/src/controllers/Controller.java
@@ -0,0 +1,18 @@
+/*  Supply Chain Central Application
+    Michael Bernard, Benjamin Chopson, Vasily Kushakov
+    CSCI 3321
+    Controller
+*/
+
+package controllers;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class Controller {
+    
+    public static void main(String[] args) {
+        
+    }
+}
