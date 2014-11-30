3d555ada10bf0a3aa4634003bea76d32fff937db
Benjamin <Benjamin@Benjamin-HP>
2014-11-29 22:40:12 -0500

    Merge origin/master
    Conflicts:
    	SupplyChainCentral/src/windows/ShipmentWindow.java

---
+++ SupplyChainCentral/src/controllers/ReportingController.java
@@ -0,0 +1,25 @@
+/*  Supply Chain Central Application
+    Michael Bernard, Benjamin Chopson, Vasily Kushakov
+    CSCI 3321
+    ReportingController
+ */
+package controllers;
+
+import databaseconnection.DatabaseConnection;
+import tableobjects.User;
+import windows.ReportingWindow;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class ReportingController {
+    public ReportingWindow reportingWindow;
+    public DatabaseConnection dbConn;
+    User user;
+    
+    public ReportingController(DatabaseConnection dbConn) {
+        this.dbConn = dbConn;
+        reportingWindow = new ReportingWindow();
+    }
+}
